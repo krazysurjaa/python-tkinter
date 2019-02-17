cb27a858a5a562483a20ff46927c663f9d83cc44
krazysurjaa <44291967+krazysurjaa@users.noreply.github.com>
2019-02-17 10:30:42 -0800

    Create window.py

---
+++ window.py
@@ -0,0 +1,15 @@
+#create a window
+
+import tkinter as tk
+root = tk.Tk()
+#
+#this is sufficient to create window 
+#but you can set geometry of main window
+root.geometry('600x500')
+
+#this will set backgroundcolor as lightgreen
+
+root['bg'] = 'lightgreen'
+#
+mainloop()
+
