01d85cfd3223a7ea046ca599f7276c93680c1ead
krazysurjaa <44291967+krazysurjaa@users.noreply.github.com>
2019-02-18 10:19:29 -0800

    Create image.py

---
+++ image.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+logo = tk.PhotoImage(file="RE.gif")
+
+w1 = tk.Label(root, image=logo).pack(side="right")
+
+explanation = """At present, only GIF and PPM/PGM
+formats are supported, but an interface 
+exists to allow additional image file
+formats to be added easily."""
+
+w2 = tk.Label(root, 
+              justify=tk.LEFT,
+              padx = 10, 
+              text=explanation).pack(side="left")
+root.mainloop()
