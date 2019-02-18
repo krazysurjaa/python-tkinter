a92cff7e7674ee9d30d29a0814d1a64b7589f2a9
krazysurjaa <44291967+krazysurjaa@users.noreply.github.com>
2019-02-18 10:21:24 -0800

    Create entry.py

---
+++ entry.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+
+master = Tk()
+Label(master, text="First Name").grid(row=0)
+Label(master, text="Last Name").grid(row=1)
+
+e1 = Entry(master).grid(row=0,column=1)
+e2 = Entry(master).grid(row=1,column=1)
+
+#e1.grid(row=0, column=1)
+#e2.grid(row=1, column=1)
+
+mainloop( )
